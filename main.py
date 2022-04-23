21efc50f8d572d3d49b5c842d982bf831900dba5
lq <1464445232@qq.com>
2022-04-24 01:46:32 +0800

    修改主函数读取启动参数创建文档

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
+import sys
+
 from data_loader import *
 from doc_processor import DocumentProcessor
-from template_manager import TemplateManager
+# from template_manager import TemplateManager
 
 
 def main():
-    data_loader = JsonDataLoader(file_path='data/json_data.txt')
-    TemplateManager.load_info('data/.index')
+    args = sys.argv[1:]
+    help_str = '参数格式为：模板路径，生成目录，文档名称，数据文件路径。'
+    if len(args) == 1 and args[0] == '-h':
+        print(help_str)
+        return
+    elif len(args) == 4:
+        t_p, d_p, d_n, j_p = args
+    else:
+        print('没有提供创建文档参数或参数错误。 ' + help_str)
+        return
 
-    tem_path = 'data/template-demo.docx'
-    to_dir = "test_data"
+    # data_loader = JsonDataLoader(file_path='data/json_data.txt')
+    data_loader = JsonDataLoader(file_path=j_p)
+    # TemplateManager.load_info('data/.index')
 
+    # tem_path = 'data/template-demo.docx'
+    tem_path = t_p
+    # to_dir = "test_data"
+    to_dir = d_p
+
+    # 通过魔法数据传递指定的模板参数
     data = data_loader.load_data()
-    DocumentProcessor.create_doc_specify_info(to_dir, data, tem_path)
+    DocumentProcessor.create_doc_specify_info(to_dir, data, tem_path=tem_path, doc_name=d_n)
 
 
 if __name__ == '__main__':

6cca903ea96d3117ff5919f21c32afad6fec2c39
lq <1464445232@qq.com>
2022-04-24 15:35:42 +0800

    通过文件方式输出执行结果

--- __init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = "0.1.0"
-
-from doc_processor import DocumentProcessor
-from template_manager import TemplateManager
-from magic_data import MagicData
-from data_loader import DataLoader, StaticDataLoader, JsonDataLoader
--- main.py
+++ main.py
@@ -5,17 +5,25 @@ from doc_processor import DocumentProcessor
 # from template_manager import TemplateManager
 
 
+def write_result_to_file(file_path, result: [str, int, bool]):
+    with open(file_path, 'w') as f:
+        f.write(str(result))
+
+
 def main():
+    sys.stdout = None
+    sys.stderr = None
+
     args = sys.argv[1:]
-    help_str = '参数格式为：模板路径，生成目录，文档名称，数据文件路径。'
+    help_str = '参数格式为：模板路径，生成目录，文档名称，数据文件路径，结果输出文件路径。'
     if len(args) == 1 and args[0] == '-h':
         print(help_str)
-        return
-    elif len(args) == 4:
-        t_p, d_p, d_n, j_p = args
+        return False
+    elif len(args) == 5:
+        t_p, d_p, d_n, j_p, r_p = args
     else:
         print('没有提供创建文档参数或参数错误。 ' + help_str)
-        return
+        return False
 
     # data_loader = JsonDataLoader(file_path='data/json_data.txt')
     data_loader = JsonDataLoader(file_path=j_p)
@@ -28,7 +36,14 @@ def main():
 
     # 通过魔法数据传递指定的模板参数
     data = data_loader.load_data()
-    DocumentProcessor.create_doc_specify_info(to_dir, data, tem_path=tem_path, doc_name=d_n)
+    if data is not None:
+        result = DocumentProcessor.create_doc_specify_info(to_dir, data, tem_path=tem_path, doc_name=d_n)
+        write_result_to_file(r_p, int(result))
+        return result
+    else:
+        print('数据为空，无法创建文档。')
+        write_result_to_file(r_p, int(False))
+        return False
 
 
 if __name__ == '__main__':
